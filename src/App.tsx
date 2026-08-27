1036b3b9cba1d3eaa33c27681e52273fc1006877
Essam Abo Elmgd <essamaboelmgd1@gmail.com>
2026-08-27 20:56:17 +0300

    feat: add Premium Signal section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ShiftHero from './components/ShiftHero'
+import PremiumSignalSection from './components/PremiumSignalSection'
 
 export default function App() {
   return (
     <div className="site-shell">
       <ShiftHero />
+      <PremiumSignalSection />
     </div>
   )
 }
